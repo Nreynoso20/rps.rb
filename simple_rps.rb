ab0db10c96b05c3e502defbb2aa712e01ca5ff33
Nreynoso20 <nathan_reynoso@yahoo.com>
2014-04-02 17:56:59 +0000

    Started working on simple rock,paper,scissors game

---
+++ simple_rps.rb
@@ -0,0 +1,4 @@
+OUTCOMES = {"rock" => {"rock" => "Tie", "paper" => "Player two won the game !", "scissors" => "Player one won !"},
+            "paper" => {},
+            "scissors" => {}
+           }
