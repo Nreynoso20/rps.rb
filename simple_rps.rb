3905a9d7e6de1816f72875bc4b80ebcec0d2cba8
Nreynoso20 <nathan_reynoso@yahoo.com>
2014-04-11 17:32:03 +0000

    Made changes to our code to make it simple

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,4 +1,12 @@
 OUTCOMES = {"rock" => {"rock" => "Tie", "paper" => "Player two won the game !", "scissors" => "Player one won !"},
-            "paper" => {},
-            "scissors" => {}
-           }
+            "paper" => {"paper" => "Tie", "rock" => "Player one won!", "scissors" => "Player two won!"},
+            "scissors" => {"scissors" => "Its a tie,try again!", "paper" => "Player one won!", "rock" => "player two won this time!"}
+            }
+          
+
+  computer_choice = ["paper", "rock", "scissors"].sample
+puts "Pick a choice my good friend. Choose rock, paper, or scissors!"
+users_choice = gets.chomp
+puts OUTCOMES[users_choice][computer_choice]
+
+
